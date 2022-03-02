47d2ef36c764a4a93c5273f05384058b8b844ba1
Timur <2001rich04@gmail.com>
2022-03-02 20:32:06 +0300

    Setting up workspace

---
+++ firstOOPPropgramm/Homework1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstOOPPropgramm
+{
+    static class Homework1
+    {
+        public static string ChooseTask()
+        {
+            Console.WriteLine("Choose homework and task");
+            Console.WriteLine("Example: to reach first task of first homework type 1.1");
+            Console.Write("Type here: ");
+            string task = Console.ReadLine();
+            if (task == "")
+            {
+                task = "repeat";
+            }
+            return task;
+        }
+    }
+}
---
+++ firstOOPPropgramm/Program.cs
@@ -0,0 +1,22 @@
+﻿using FirstOOPPropgramm;
+
+string actualTask = FirstOOPPropgramm.Homework1.ChooseTask();
+bool isKeepWork = true;
+
+while (isKeepWork == true)
+{
+    if (actualTask == "0")
+    {
+        isKeepWork = false;
+    }
+    else if (actualTask == "repeat")
+    {
+        actualTask = FirstOOPPropgramm.Homework1.ChooseTask();
+    }
+    else if (actualTask == "clear")
+    {
+        Console.Clear();
+        actualTask = FirstOOPPropgramm.Homework1.ChooseTask();
+    }
+}
+
