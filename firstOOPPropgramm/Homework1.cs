fe746886dcd75fa84332df785ec7456f2c1ca283
Timur <2001rich04@gmail.com>
2022-03-04 23:39:08 +0300

    Add exceptions for Homework1

--- firstOOPPropgramm/Homework1.cs
+++ firstOOPPropgramm/Homework1.cs
@@ -10,43 +10,65 @@ namespace FirstOOPPropgramm
     {
         public static int GetSolution(int a, int b)
         {
-            int res = (5 * a + b * b) / (b - a);
-            return res;
+            if (a == b)
+            {
+                throw new Exception("<a> can't be equal <b>");
+            }
+            else
+            {
+                int res = (5 * a + b * b) / (b - a);
+                return res;
+            }
         }
 
         public static int GetSolveOfLinearEquation(int a, int b, int c)
         {
-            int res = (c - b) / a;
-            return res;
+            if (a == 0)
+            {
+                throw new Exception("<a> can't be equal 0");
+            }
+            else
+            {
+                int res = (c - b) / a;
+                return res;
+            }
+            
         }
 
         public static string GetEquationOfStraightLine(int x1, int y1, int x2, int y2)
         {
-            double tmp1 = (double)(y1 - y2) / (x2 - x1);
-            double tmp2 = (double)((y2 - y1) * x1) / (x2 - x1);
-            tmp1 *= -1;
-            tmp2 += -1;
-
-            string res = "";
-
-            if (tmp2 == 0 && tmp1 == 0)
+            if (x2 == x1)
             {
-                res = "Y = 0";
-            }
-            else if (tmp2 == 0)
-            {
-                res = $"Y = {tmp1}X";
-            }
-            else if (tmp1 == 0)
-            {
-                res = $"Y = {tmp2}";
+                throw new Exception("<x1> can't be equal <x2>");
             }
             else
             {
-                res = $"Y = {tmp1}x + {tmp2}";
-            }
+                double tmp1 = (double)(y1 - y2) / (x2 - x1);
+                double tmp2 = (double)((y2 - y1) * x1) / (x2 - x1);
+                tmp1 *= -1;
+                tmp2 += -1;
 
-            return res;
+                string res = "";
+
+                if (tmp2 == 0 && tmp1 == 0)
+                {
+                    res = "Y = 0";
+                }
+                else if (tmp2 == 0)
+                {
+                    res = $"Y = {tmp1}X";
+                }
+                else if (tmp1 == 0)
+                {
+                    res = $"Y = {tmp2}";
+                }
+                else
+                {
+                    res = $"Y = {tmp1}x + {tmp2}";
+                }
+
+                return res;
+            }
         }
         public static int GetResultOfDivison(int a, int b)
         {
