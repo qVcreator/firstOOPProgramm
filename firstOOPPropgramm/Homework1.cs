3459b767d20b05f099bfd0fa3431b7ddc1ed3601
Timur <2001rich04@gmail.com>
2022-03-04 12:47:42 +0300

    Add new method named GetResult in Class named Homework 1

--- firstOOPPropgramm/Homework1.cs
+++ firstOOPPropgramm/Homework1.cs
@@ -48,5 +48,16 @@ namespace FirstOOPPropgramm
 
             return res;
         }
+        public static int GetResultOfDivison(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new Exception("b haven't to be zero");
+            }
+            else
+            {
+                return a / b;
+            }
+        }
     }
 }
