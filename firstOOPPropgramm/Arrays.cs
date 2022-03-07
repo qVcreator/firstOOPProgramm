13fdfa0553bf6edc6bddd173543558b656b154f2
Timur <2001rich04@gmail.com>
2022-03-07 13:45:01 +0300

    Add FindMinOfArray method in Array class

---
+++ firstOOPPropgramm/Arrays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstOOPPropgramm
+{
+    static class Arrays
+    {
+        public static int FindMinOfArray(int[] arr)
+        {
+            if (arr == null)
+            {
+                throw new Exception("arr can't be null");
+            }
+
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+    }
+}
