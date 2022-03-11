e25e342973ec35ea4daa4eae5bdcaeb8041c14fc
Timur <2001rich04@gmail.com>
2022-03-11 15:42:45 +0300

    Add positive expectations for GetSolutionTest

--- FirstOOPProgramm.Test/Homework1Tests.cs
+++ FirstOOPProgramm.Test/Homework1Tests.cs
@@ -1,16 +1,20 @@
 using NUnit.Framework;
 using System;
-using FirstOOPPropgramm;
 
-namespace FirstOOPProgramm.Test
+namespace FirstOOPPropgramm.Test
 {
     public class Homework1Tests
     {
 
-        [Test]
+        [TestCase(5, 7, 37)]
+        [TestCase(-5 ,5, 0)]
+        [TestCase(0, 5, 5)]
+        [TestCase(5, 0, -5)]
+        [TestCase(-10, 5, -5.0/3)]
         public void GetSolutionTest(double a, double b, double expected)
         {
-            double actual = FirstOOPPropgramm.Homework1
+            double actual = Homework1.GetSolution(a, b);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
