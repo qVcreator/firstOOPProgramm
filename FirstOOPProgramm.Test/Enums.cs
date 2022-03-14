15dfe968b04910008ab6b905d0fedc186505c1a7
Timur <2001rich04@gmail.com>
2022-03-14 13:05:25 +0300

    Add GetMock

--- FirstOOPProgramm.Test/Enums.cs
+++ FirstOOPProgramm.Test/Enums.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstOOPProgramm.Test
+namespace FirstOOPPropgramm.Test
 {
     public static class Enums
     {
---
+++ FirstOOPProgramm.Test/TwoDimensionalArrayMock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+namespace FirstOOPPropgramm.Test
+{
+    public static class TwoDimensionalArrayMock
+    {
+        public static int[,] GetMock(FirstOOPPropgramm.Test.Enums.TwoDimensionalArraay type)
+        {
+            switch (type)
+            {
+                case:
+                    return;
+                    break;
+            }
+        }
+    }
+}
